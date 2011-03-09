0da5f640c899f8064af055155cdc53446bf2346b
WhenRaptorsAttack <pykedout@gmail.com>
2011-03-08 21:44:29 -0800

    Removed old OOP helpers

--- jixel.js
+++ jixel.js
@@ -1,31 +1,4 @@
-Function.prototype.Inherits = function (fnSuper) {
-    var prop;
-    if (this == fnSuper) {
-        alert("Error - cannot derive from self");
-        return;
-    }
-    for (prop in fnSuper.prototype) {
-    if (typeof(fnSuper.prototype[prop]) == "function" && !this.prototype[prop]) {
-        this.prototype[prop] = fnSuper.prototype[prop];
-        }
-    }
-    this.prototype[fnSuper.StName()] = fnSuper;
-}
-
-Function.prototype.StName = function () {
-    var st;
-    st = this.toString();
-    st = st.substring(st.indexOf(" ")+1, st.indexOf("("));
-    if (st.charAt(0) == "(")
-        st = "function ...";
-    return st;
-}
-Function.prototype.Override = function (fnSuper, stMethod) {
-    this.prototype[fnSuper.StName() + "_" + stMethod] = fnSuper.prototype[stMethod];
-}
-
 var objID = 0;
-
 var JxlState = new Class({
     initialize: function() {
         this.defaultGroup = new JxlGroup();
